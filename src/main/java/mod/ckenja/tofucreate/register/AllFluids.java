9f8bde03140e87808cbc2fa4ccb3e5d452a4e383
Polaris_Light <49135577+GLDYM@users.noreply.github.com>
2024-12-04 10:01:26 +0800

    Update AllFluids.java

--- src/main/java/mod/ckenja/tofucreate/register/AllFluids.java
+++ src/main/java/mod/ckenja/tofucreate/register/AllFluids.java
@@ -50,7 +50,7 @@ public class AllFluids {
     public static final FluidEntry<VirtualFluid> SOYMILK_RAMUNE =
             registrate.virtualFluid("soymilk_ramune", new ResourceLocation(TofuCraftReload.MODID, "block/soymilk"), new ResourceLocation(TofuCraftReload.MODID, "block/soymilk_flow"), create(0xFF9EC4FB, 0x9EC4FB), VirtualFluid::new)
                     .register();
-    public static final FluidEntry<VirtualFluid> SOYMILK_RAMUNE =
+    public static final FluidEntry<VirtualFluid> SOYOIL =
             registrate.virtualFluid("soyoil", new ResourceLocation(TofuCraftReload.MODID, "block/soyoil"), new ResourceLocation(TofuCraftReload.MODID, "block/soyoil_flow"), create(0x00FFFFFF, 0x00FFFF), VirtualFluid::new)
                     .register();
     public static void register() {}
